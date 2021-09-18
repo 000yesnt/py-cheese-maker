a18fecde549c75d73e5f119cf884b7d0aae65df1
000yesnt <49329895+000yesnt@users.noreply.github.com>
2021-09-17 22:55:43 -0300

    i think we're ready for release????

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
+#!/usr/bin/env python3
+import time
+import shutil
+import argparse
+import glob
+import os
+import subprocess
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import CheeseFactory
 
+class loglevels: # we'll use this one day
+    warn = "[ \033[93mWARN\033[0m ]"
+    err = "[\033[91mFAILED\033[0m]"
+    ok = "[\033[92m  OK  \033[0m]"
+    f_scroll = ["[\033[91m*     \033[0m]",
+                "[\033[31m*\033[91m*    \033[0m]",
+                "[\033[91m*\033[31m*\033[91m*   \033[0m]",
+                "[ \033[91m*\033[31m*\033[91m*  \033[0m]",
+                "[  \033[91m*\033[31m*\033[91m* \033[0m]",
+                "[   \033[91m*\033[31m*\033[91m*\033[0m]",
+                "[    \033[91m*\033[31m*\033[0m]",
+                "[     \033[91m*\033[0m]",
+                "[    \033[91m*\033[31m*\033[0m]",
+                "[   \033[91m*\033[31m*\033[91m*\033[0m]",
+                "[  \033[91m*\033[31m*\033[91m* \033[0m]",
+                "[ \033[91m*\033[31m*\033[91m*  \033[0m]",
+                "[\033[91m*\033[31m*\033[91m*   \033[0m]",
+                "[\033[31m*\033[91m*    \033[0m]"]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+psr = argparse.ArgumentParser(prog="'Top X Cheese' maker",
+                              description="This script generates those funny 'top 50000 cheese' memes that are all over Discord now from input files.")
 
+psr.add_argument('--path', help="path to use as input", type=str, default="in/")
+psr.add_argument('-s', help="string to use at the start, supports formatting ({ln} = amount of files in input)",
+                 type=str, default="top {ln} images")
+psr.add_argument('-r', help="framerate of output video", type=int, default=1)
+psr.add_argument('-o', help="name of output mp4", type=str, default="result")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+args = psr.parse_args()
+if not os.path.exists(args.path):
+    raise Exception("Path does not exist")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if shutil.which("ffmpeg") is None:
+    raise Exception("You forgot ffmpeg")
+
+imlist = glob.glob(f"{args.path}/*.png") + glob.glob(f"{args.path}/*.jpg")
+u = len(imlist)
+FrameOrder = [
+    CheeseFactory.MakeCheeseFrame(1, (256, 256), text=str(args.s).format(ln=len(imlist))),
+    CheeseFactory.MakeCheeseFrame(1, (256, 256), text=f"number {u}")
+]
+
+print("Making frames")
+for f in imlist:
+    FrameOrder.append(CheeseFactory.MakeCheeseFrame(2, (256, 256), imagepath=f))
+    u -= 1
+    if u == 0:
+        break
+    FrameOrder.append(CheeseFactory.MakeCheeseFrame(0, (256, 256), text=f"number {u}"))
+
+print("Writing frames")
+imindex = 0
+for img in FrameOrder:
+    img.save(f"out/imindex{imindex}.png")
+    imindex += 1
+
+print("Processing with ffmpeg")
+subprocess.run(["ffmpeg", "-hide_banner", "-framerate", str(args.r), "-y", "-i", "out/imindex%d.png", "-c", "libx264", f"out/{args.o}.mp4"], capture_output=True)
+print("Cleaning up")
+
+tg = glob.glob(f"out/*.png")
+for g in tg:
+    os.remove(g)
+
+print("Done")
